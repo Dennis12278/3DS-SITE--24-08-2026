1fa978df8fce67cfae8710c72c943ce766109a33
Dennis12278 <dennissilvageneroso@gmail.com>
2026-08-25 20:39:12 -0300

    perfil Exp.0.2

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,20 +1,48 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+
 import { UserMenuComponent } from '../user-menu/user-menu';
 
 @Component({
   selector: 'app-header',
+
   standalone: true,
-  imports: [CommonModule, UserMenuComponent],
+
+  imports: [
+    CommonModule,
+    UserMenuComponent
+  ],
+
   templateUrl: './header.html',
+
   styleUrl: './header.css'
 })
+
 export class HeaderComponent {
 
   userMenuOpen = false;
 
-  toggleUserMenu(): void {
+  constructor(private router: Router) {}
+
+  toggleUserMenu(event: MouseEvent): void {
+
+    event.stopPropagation();
+
     this.userMenuOpen = !this.userMenuOpen;
+
+  }
+
+  fecharUserMenu(): void {
+
+    this.userMenuOpen = false;
+
+  }
+
+  irParaInicio(): void {
+
+    this.router.navigate(['/']);
+
   }
 
 }
--- src/app/components/user-menu/user-menu.ts
+++ src/app/components/user-menu/user-menu.ts
@@ -1,19 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-menu',
+
   standalone: true,
+
   imports: [],
+
   templateUrl: './user-menu.html',
+
   styleUrl: './user-menu.css'
 })
+
 export class UserMenuComponent {
+
+  @Output() fecharMenu = new EventEmitter<void>();
 
   constructor(private router: Router) {}
 
-  irParaMeuPerfil() {
+  irParaMeuPerfil(): void {
+
+    this.fecharMenu.emit();
+
     this.router.navigate(['/meu-perfil']);
+
+  }
+
+  fechar(): void {
+
+    this.fecharMenu.emit();
+
   }
 
 }

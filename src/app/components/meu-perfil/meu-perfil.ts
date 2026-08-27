3c8012dd7228916c0bc24fe77dec2c98e094dc99
Dennis12278 <dennissilvageneroso@gmail.com>
2026-08-26 21:56:55 -0300

    perfil Exp.0.3

--- src/app/components/meu-perfil/meu-perfil.ts
+++ src/app/components/meu-perfil/meu-perfil.ts
@@ -2,10 +2,34 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-meu-perfil',
+  standalone: true,
   imports: [],
   templateUrl: './meu-perfil.html',
   styleUrl: './meu-perfil.css',
 })
 export class MeuPerfil {
 
+  avatarUrl: string | null = null;
+
+  selecionarAvatar(event: Event): void {
+    const input = event.target as HTMLInputElement;
+
+    if (!input.files || input.files.length === 0) {
+      return;
+    }
+
+    const arquivo = input.files[0];
+
+    if (!arquivo.type.startsWith('image/')) {
+      return;
+    }
+
+    const leitor = new FileReader();
+
+    leitor.onload = () => {
+      this.avatarUrl = leitor.result as string;
+    };
+
+    leitor.readAsDataURL(arquivo);
+  }
 }
